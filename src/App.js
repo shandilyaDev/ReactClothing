ca09e2c3f6960dc5fd25e04ba3d33a5112e550f3
shandilyaDev <shandilya@Ramendus-MacBook-Pro.local>
2020-04-08 12:14:56 +0530

    Added Sign In Sign Up Page

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component";
 import "./App.css";
 import HeaderComponent from "./components/header/header.component";
 
 function App() {
   return (
     <div>
-    
       <BrowserRouter>
-      <HeaderComponent />
+        <HeaderComponent />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInSignUpPage} />
         </Switch>
       </BrowserRouter>
     </div>
---
+++ src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SignInComponent from "../../components/sign-in/sign-in.component";
+import "./sign-in-sign-up.styles.scss";
+
+const SignInSignUpPage = () => (
+  <div className="sign-in-sign-up">
+    <SignInComponent />
+  </div>
+);
+
+export default SignInSignUpPage;
